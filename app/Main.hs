9af182f825bd93f2cd707bcf645eab767e1f2683
Alex Chapman <alex@farfromthere.net>
2018-02-04 15:04:35 +1100

    Use optparse-applicative to read the name of the file to process

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,28 @@
 module Main where
 
+import Options.Applicative
+import Data.Semigroup ((<>))
+
 import CSVQuery
 
+data Options = Options
+    { csvfile :: FilePath
+    }
+
+options :: Parser Options
+options = Options
+            <$> strArgument
+                ( metavar "FILE"
+                <> help "The CSV file to read" )
+
 main :: IO ()
-main = do
-    table <- readCSVTableFile "test/CSVfile.csv"
+main = run =<< execParser opts
+    where opts = info (options <**> helper)
+                      (fullDesc
+                         <> progDesc "Read FILE as a CSV file and print its contents or answer queries about it"
+                         <> header "csvquery - a CSV file querier")
+
+run :: Options -> IO ()
+run (Options f) = do
+    table <- readCSVTableFile f
     print $ show table
