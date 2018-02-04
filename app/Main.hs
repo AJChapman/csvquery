3edc6b5d2809bb159f0ba588b2981c9f976a6718
Alex Chapman <alex@farfromthere.net>
2018-02-04 18:32:25 +1100

    Add field argument, to show a single field

--- app/Main.hs
+++ app/Main.hs
@@ -12,7 +12,7 @@ import Table
 data Options = Options
     { csvfile :: FilePath
     , filter  :: String
-    }
+    , field   :: String }
 
 options :: Parser Options
 options = Options
@@ -21,10 +21,14 @@ options = Options
                 <> help "The CSV file to read")
             <*> strOption
                 (long "filter"
-                <> short 'f'
                 <> metavar "FILTER"
                 <> value "*"
                 <> help "An optional row filter, e.g. \"Name=Dave\". If not specified then all rows will be displayed.")
+            <*> strOption
+                (long "field"
+                <> metavar "FIELD"
+                <> value ""
+                <> help "Select a single field to display. If not specified then the data will be displayed in a table.")
 
 main :: IO ()
 main = run =<< execParser opts
@@ -34,8 +38,10 @@ main = run =<< execParser opts
                          <> header "csvquery - a CSV file querier")
 
 run :: Options -> IO ()
-run (Options file filterString) = do
+run (Options file filterString field) = do
     table  <- readCSVTableFile file
     filter <- parseFilter (T.pack filterString)
     table' <- applyFilter filter table
-    printTable table'
+    if field == ""
+       then printTable table'
+       else printField (T.pack field) table'
--- src/Print.hs
+++ src/Print.hs
@@ -1,6 +1,7 @@
 module Print
     ( renderTable
     , printTable
+    , printField
     ) where
 
 import Data.List
@@ -15,8 +16,19 @@ boxTable t =
         cols = tableAsColumns t'
      in hsep 2 left (map (vcat left) cols)
 
+-- | Render the table to its ascii form
 renderTable :: Table T.Text -> String
 renderTable = render . boxTable
 
+-- | Print the table in its ascii form
 printTable :: Table T.Text -> IO ()
 printTable = printBox . boxTable
+
+boxColumn :: [T.Text] -> Box
+boxColumn = vcat left . (fmap (text . T.unpack))
+
+-- | Print the column with the given heading
+printField :: T.Text -> Table T.Text -> IO ()
+printField c t = do
+    col <- getColumn c t
+    printBox (boxColumn col)
