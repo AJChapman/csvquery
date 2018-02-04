6829b2c7fd8052d17f3900918364f4421e66a851
Alex Chapman <alex@farfromthere.net>
2018-02-04 13:45:38 +1100

    Move table errors to their own type, implement filterTable, getRow, getColumn.

--- src/CSVError.hs
+++ src/CSVError.hs
@@ -1,5 +1,5 @@
 module CSVError
-    ( CSVError(InvalidCSV, TabularisationError)
+    ( CSVError(InvalidCSV)
     ) where
 
 import Control.Exception
@@ -7,13 +7,11 @@ import Data.Typeable
 import Text.Megaparsec (ShowErrorComponent, showErrorComponent)
 
 data CSVError = InvalidCSV FilePath String
-              | TabularisationError String
               deriving (Eq, Ord, Typeable)
 
 -- Tell Haskell how to print our errors.
 instance Show CSVError where
     show (InvalidCSV file msg) = "Error when trying to parse file '" ++ file ++ "' as csv: " ++ msg
-    show (TabularisationError msg) = "Unable to tabularise data: " ++ msg
 
 -- Make CSVError throwable.
 instance Exception CSVError
--- src/Table.hs
+++ src/Table.hs
@@ -2,15 +2,17 @@
 module Table
     ( Row(Row, rCells)
     , Table(Table, tHeaders, tRows)
-    , rowCount, columnCount
     , tabularise
+    , rowCount, columnCount, rowLength
+    , filterTable
+    , getRow, getColumn
     ) where
 
 import Control.Monad
 import Control.Monad.Catch
 import qualified Data.Text as T
-
-import CSVError
+import Data.List
+import Data.Typeable
 
 -- | A row in the table.
 newtype Row a = Row { rCells :: [a] } deriving (Eq, Show, Foldable)
@@ -28,9 +30,56 @@ rowCount = length . tRows
 columnCount :: Table a -> Int
 columnCount = length . tHeaders
 
--- | Take a list of rows and turn them into a table by treating the
--- first row as a header row. Throws a TabularisationError if no
--- header row can be found.
+-- | The number of cells in the row.
+rowLength :: Row a -> Int
+rowLength = length . rCells
+
+data TableError = NoRowsError
+                | ColumnCountError Int
+                | UnknownColumnError String
+                | RowIndexOutOfBoundsError Int Int
+                deriving (Eq, Ord, Typeable)
+
+instance Show TableError where
+    show NoRowsError
+      = "No rows found, so none could be assigned as the table header."
+    show (ColumnCountError n)
+      = "Rows do not all have the same number of columns as the header, which has " ++ show n ++ "."
+    show (UnknownColumnError h)
+      = "Could not find a column labeled '" ++ h ++ "'."
+    show (RowIndexOutOfBoundsError i n)
+      = "Could not find row " ++ show i ++ ", as there are only " ++ show n ++ " rows."
+
+instance Exception TableError
+
+-- | Take a list of rows and turn them into a table by treating the first row
+-- as a header row. Throws a TableError if no header row can be found, or if
+-- the number of items in any row does not match the number of columns in the
+-- header.
 tabularise :: MonadThrow m => [ Row a ] -> m (Table a)
-tabularise []     = throwM (TabularisationError "No rows found, so none could be assigned as the table header")
-tabularise (x:xs) = return (Table x xs)
+tabularise []     = throwM NoRowsError
+tabularise (x:xs) =
+    let n = length x in
+        if (all (lengthIs n) xs)
+           then return (Table x xs)
+           else throwM (ColumnCountError n)
+       where lengthIs :: Int -> Row a -> Bool
+             lengthIs n r = n == (rowLength r)
+
+-- | Filter the table to contain only rows matching the given predicate.
+filterTable :: (Row a -> Bool) -> Table a -> Table a
+filterTable f (Table h rs) = Table h (filter f rs)
+
+getRow :: MonadThrow m => Int -> Table a -> m (Row a)
+getRow i (Table _ rs) =
+    let n = length rs
+     in if (i >= n)
+           then throwM (RowIndexOutOfBoundsError i n)
+           else return (rs !! i)
+
+getColumn :: (MonadThrow m, Eq a, Show a) => a -> Table a -> m [a]
+getColumn c (Table h rs) =
+    let mi = elemIndex c (rCells h)
+     in case mi of
+          Nothing -> throwM (UnknownColumnError (show c))
+          Just i -> return (map ((!! i) . rCells) rs)
