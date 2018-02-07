fa989b7a6896f5c33f8335f31cade77d8da40d96
Alex Chapman <alex@farfromthere.net>
2018-02-08 08:51:06 +1100

    Add a test for Dave's phone number

--- test/Spec.hs
+++ test/Spec.hs
@@ -18,9 +18,6 @@ readTestFile f = readCSVTableFile f
 testRows table = TestCase (assertEqual "four rows in table" 4 (rowCount table))
 testCols table = TestCase (assertEqual "three columns in table" 3 (columnCount table))
 
-testFilter :: IO (Filter T.Text)
-testFilter = parseFilter "Name=Bob"
-
 testFilterParses filter =
     TestCase (assertEqual
                 "parse Name=Bob filter"
@@ -32,14 +29,24 @@ testOneBob table =
                  "filtering with Name=Bob gives us a single row"
                  (rowCount table) 1)
 
+testDavesPhoneNumber numbers =
+    TestCase (assertEqual
+                "check that Dave's phone number is what we expect"
+                ["0400 001 123"]
+                numbers)
+
 testsForFile file = do
-    filter <- testFilter
+    filterBob <- parseFilter "Name=Bob"
+    filterDave <- parseFilter "Name=Dave"
     table <- readTestFile file
-    filteredTable <- applyFilter filter table
+    filteredTableBob <- applyFilter filterBob table
+    filteredTableDave <- applyFilter filterDave table
+    daveNumbers <- getColumn "Phone" filteredTableDave
     return (TestList [ TestLabel "testRows" (testRows table)
                      , TestLabel "testCols" (testCols table)
-                     , TestLabel "testFilterParses" (testFilterParses filter)
-                     , TestLabel "testOneBob" (testOneBob filteredTable)])
+                     , TestLabel "testFilterParses" (testFilterParses filterBob)
+                     , TestLabel "testOneBob" (testOneBob filteredTableBob)
+                     , TestLabel "testDaveNumber" (testDavesPhoneNumber daveNumbers)])
 
 main :: IO Counts
 main = do
